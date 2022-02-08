ac043013c8967af2b35aa032d53488f17b09cdc6
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2022-02-08 17:32:39 -0500

    rearranged the order of tip section for better UX; fixed styles and made a dark mode and styled that; also adjusted some css; added favicon

--- script.js
+++ script.js
@@ -1,93 +1,109 @@
-// document.getElementById("left").onchange = function() {
-//     let bill = Number(document.getElementById("number_bill").value);
-//     let people = Number(document.getElementById("number_people").value);
-//     let tip = Number(document.querySelectorAll("tip_btn"));
-//     percent.forEach(function(btn) {
-//         btn.onclick = function() {
-//             console.log(btn.value);
-//         };
-        
-//     });
-//     console.log(bill, people, tip);
-
-//     let tipPerPerson = (bill * (tip/100)) / people;
-//     let totalPerPerson = tipPerPerson + (bill / people);
-
-//     if (isNaN(tipPerPerson)) {
-//     tipPerPerson = 0.00;
-//     totalPerPerson = 0.00;
-//     }
-
-
-//     document.getElementById("tip_display").innerHTML = `$${tipPerPerson.toFixed(2)}`;
-//     document.getElementById("total_display").innerHTML = `$${totalPerPerson.toFixed(2)}`;
-
-// };
-
-console.log(`
-====================== BREAK ===========================
-
-============== THIS IS CODE FROM CLASS NOTES ===========
-`);
-// console.log("HELLO WORLD")
-// document.getElementById("left").onchange = function() {
-//======================= How do I grab the bill input elements
-let bill = document.getElementById("number_bill");
-let people = document.getElementById("number_people");
-let percent = document.querySelectorAll(".tip_btn");
-// console.log(bill.value);
-// console.log(percent.value);
-// console.log(people.value);
+let bill = document.getElementById("number_bill")
+let people = document.getElementById("number_people")
+let percent = document.querySelectorAll(".tip_btn")
+let custom = document.getElementById("tip_input")
+// ========= DARK MODE START VARIBLES
+let theme = document.getElementById("theme_btn")
+let darkMode = false;
+let body = document.body.style
+let container = document.querySelector(".container").style
+let textP = document.querySelectorAll("p")
+let totalText = document.querySelectorAll(".total_text")
+let textPersonSub = document.querySelectorAll(".person_sub")
+let inputContainers = document.querySelectorAll(".input_container")
+let tipButton = document.querySelectorAll(".tip_btn")
+let rightContainer = document.querySelector(".right").style
+// =========  DARK MODE END VARIBLES 
 
 
 function tipSplit(billAmount, amtPeople, percentage) {
-    //================= Write code below
 
     billAmount = parseInt(bill.value)
     amtPeople = parseInt(people.value)
 
-    console.log(billAmount,amtPeople,percentage)
+    if (custom.value) {
+        percentage = parseInt(custom.value) / 100
+        if (billAmount && amtPeople) {
+            let tip = (billAmount * percentage) / amtPeople
+            let total = tip + (billAmount / amtPeople)
+            document.getElementById("tip_display").innerText = `$${tip.toFixed(2)}`;
+            document.getElementById("total_display").innerText = `$${total.toFixed(2)}`;
+        }
+    }
 
-    //====================== Write a condition that check if all 3 inputs are there?
-
-    if (billAmount && amtPeople && percentage) {
-        let tip = (billAmount * (percentage/100)) / amtPeople;
-        let total = tip + (billAmount / amtPeople);
-        console.log(tip, total)
-        document.getElementById("tip_display").innerHTML = `$${tip.toFixed(2)}`;
-        document.getElementById("total_display").innerHTML = `$${total.toFixed(2)}`;
-    } 
-    // else {
-    //     document.getElementById("tip_display").innerHTML = `$${0.00}`;
-    // }
-    
-    let tipPerPerson = (billAmount * percentage) / amtPeople
-    
-    let totalPerPerson = tipPerPerson + (billAmount / amtPeople)
-
+    else if (billAmount && amtPeople && percentage) {
+        let tip = (billAmount * percentage) / amtPeople
+        let total = tip + (billAmount / amtPeople)
+        document.getElementById("tip_display").innerText = `$${tip.toFixed(2)}`;
+        document.getElementById("total_display").innerText = `$${total.toFixed(2)}`;
+    }
 }
 
-
-console.log(tipSplit(100, 2, .20));
-
-
-bill.addEventListener("change", tipSplit)
-
-//==================== How do I grab the people input element?
-
-
-people.addEventListener("change", tipSplit)
-
-//======================= How do I make it so when I click each button it will console log the percent
-
-percent.forEach((btn)=> {
-    btn.addEventListener("click", function() {
-        let btns = parseInt(btn.value)/100;
+percent.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        let btns = parseInt(btn.value) / 100;
         tipSplit(bill, people, btns)
     })
 })
 
+let current = document.getElementsByClassName("active");
+for (var i = 0; i < percent.length; i++) {
+    percent[i].addEventListener("click", function () {
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+    });
+}
 
-// };
-console.log(`
-====================== BREAK ===========================`);
+bill.addEventListener("change", tipSplit)
+people.addEventListener("change", tipSplit)
+custom.addEventListener("change", tipSplit)
+// ==================================
+// ================== DARK MODE START
+theme.addEventListener("click", function () {
+    if (darkMode == false) {
+        darkModeProps()
+        darkMode = true;
+    } else {
+        lightModeProps()
+        darkMode = false;
+    }
+})
+function lightModeProps() {
+    body.backgroundColor = "#00526d";
+    theme.src = "./images/icon-moon.svg";
+    container.backgroundColor = "#7e8287";
+    textP.forEach(function (p) {
+        p.style.color = "#000000";
+    })
+    textPersonSub.forEach(function (p) {
+        p.style.color = "rgb(176, 167, 153)";
+    })
+    inputContainers.forEach(function (input) {
+        input.style.backgroundColor = "#F6F7FC";
+    })
+    totalText.forEach(function (p) {
+        p.style.color = "#FFFFFF";
+    })
+}
+function darkModeProps() {
+    body.backgroundColor = "rgb(62 62 63)";
+    theme.src = "./images/icon-sun.svg";
+    container.backgroundColor = "rgb(38 38 38)";
+    textP.forEach(function (p) {
+        p.style.color = "rgb(16 255 0 / 72%)";
+    })
+    textPersonSub.forEach(function (p) {
+        p.style.color = "#c9c9c9";
+    })
+    inputContainers.forEach(function (input) {
+        input.style.backgroundColor = "#4d4d4d";
+    })
+    tipButton.forEach(function (button) {
+        button.filter = "invert(1)";
+    })
+    rightContainer.backgroundColor = "rgb(77, 77, 77)";
+}
+// =================== DARK MODE END
+// =================================
